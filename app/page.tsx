3716211636be3add529f7f9587a7f9da651c4772
Areum An <areum.an.0928@gmail.com>
2025-03-16 17:09:07 -0400

    prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
+import "./lib/db"
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className="flex flex-col items-center justify-between min-h-screen p-6">
       <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
