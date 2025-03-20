96f67791c826a4f118f10dd294a5acdabfba158c
Areum An <areum.an.0928@gmail.com>
2025-03-19 21:54:44 -0400

    skeletions for products

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,7 +1,12 @@
-export default function Products() {
+async function getProducts() {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+}
+
+export default async function Products() {
+  const products = await getProducts();
   return (
     <div>
-      <h1>Products</h1>
+      <h1 className="text-white text-4xl">Products!</h1>
     </div>
-  )
+  );
 }
